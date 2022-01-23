0f4b56b3c88164378ee39489c7e40f92f1622f05
Komal Lalwani <komallalwani96@gmail.com>
2022-01-23 19:26:00 +0530

    Update variables.tf

--- webapp/variables.tf
+++ webapp/variables.tf
@@ -12,3 +12,8 @@ variable "app_service_plan_name" {
     type        = string
     description = "App Service Plan name in Azure"
 }
+
+variable "app_service_name" {
+    type        = string
+    description = "App Service name in Azure"
+}
